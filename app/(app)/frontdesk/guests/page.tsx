51007eb9839a70bacace7b0b2fb5a26ba2bbaff7
kelvin banda <120105858+kevtheprogrammer@users.noreply.github.com>
2025-05-20 23:03:36 +0200

    guess

--- app/(app)/frontdesk/guests/page.tsx
+++ app/(app)/frontdesk/guests/page.tsx
@@ -1,15 +1,12 @@
- 
- 
 import type { Metadata } from 'next'
 import CustomerListComp from './components/CustomerList'
 
 export const metadata: Metadata = {
-  title: 'Customers',
+  title: 'Guests',
+  description: 'List of guests',
 }
 
 export default async function Customers() {
-
   return <CustomerListComp  />
-    
 }
  
--- store/actions/userActions.ts
+++ store/actions/userActions.ts
@@ -1,4 +1,3 @@
-import { prisma } from "@/lib/prisma"
 import { ActionTypes } from "../constants/action-types"
 import guests from '@/app/api/apisauce/guests'
 
