a1bdc08ddf6b0a35026a325e4a65db9730cfc502
kelvin banda <120105858+kevtheprogrammer@users.noreply.github.com>
2025-06-09 09:25:33 +0200

    auth fix

--- app/(app)/frontdesk/layout.tsx
+++ app/(app)/frontdesk/layout.tsx
@@ -5,11 +5,11 @@ import React from 'react'
 import ApplicationLayout from './ApplicationLayout';
 import StoreProvider from '@/context/StoreProvider';
 import AuthProvider from '@/context/AuthProvider';
-import { useSession } from 'next-auth/react';
+import { auth } from '@/auth';
 
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
-    const {data:session} = useSession()
+    const session = await auth();
 
     return <StoreProvider>
         <AuthProvider session={session} >
